7ae66c185baed7b45403e7cb551bc996b13a8e88
Dean <463575680@qq.com>
2015-05-31 20:57:50 +0800

    Scaffold Chatroom
    创建聊天室

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,7 @@
 class Chatroom < ActiveRecord::Base
+	validates :roomname, :privacy, presence: true
+	validates :roomcover, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
