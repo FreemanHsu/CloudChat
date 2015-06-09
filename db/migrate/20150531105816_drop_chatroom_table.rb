09152b4c748eb128c86b3f1bcd8c1909ac836c87
AndyHsu <andy.xu1994@gmail.com>
2015-06-09 17:00:01 +0800

    migration bug fixed

--- db/migrate/20150531105816_drop_chatroom_table.rb
+++
@@ -1,9 +0,0 @@
-class DropChatroomTable < ActiveRecord::Migration
-  def up
-    drop_table :chatrooms
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20150603025853_change_default_of_chatroom.rb
+++ db/migrate/20150603025853_change_default_of_chatroom.rb
@@ -3,5 +3,5 @@ class ChangeDefaultOfChatroom < ActiveRecord::Migration
   	change_column :chatrooms, :roomcover, :string, :default => "assets/images/ruby.jpg"  
   	change_column :chatrooms, :popularity, :integer, :default => 1  
   	change_column :chatrooms, :memnum, :integer, :default => 1  
-  	
   end
+end
