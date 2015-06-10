028acfb144a2c23cf0e58ca36f49b220c026dfcf
AndyHsu <andy.xu1994@gmail.com>
2015-06-11 01:34:26 +0800

    What the f...

---
+++ app/controllers/chat_app_controller.rb
@@ -0,0 +1,3 @@
+class ChatAppController < ApplicationController
+	
+end
---
+++ test/controllers/chat_app_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChatAppControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
